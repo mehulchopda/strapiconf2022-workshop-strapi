1654e8cab2ca2ea674c6446b176f20fe5a21baa5
chopadamehul000@gmail.com <chopadamehul000@gmail.com>
2023-03-24 16:48:25 +0100

    Fix sendMessage api

--- src/index.js
+++ src/index.js
@@ -49,8 +49,17 @@ module.exports = {
           },
         };
         var axios = require("axios");
+        const token = '9500eb3cbc4ec39402d929d8d6a921be4bdf089a50026c1d21fc8a66aec54c6a892a23e9faf9fb02f669d3f83fef571a0252e0cc96a35044ad61ccb0f98d6ad5bc5b52d8248b3958fd04973b01700042bb5ff10b63bda7e73e584c7b2eaf380bfc48bdd54f84d20593b1bcb1c8262119978edf5e755ca5853048791285fb2f54'
+        let axiosConfig = {
+          headers: {
+              "Content-Type": "application/json;charset=UTF-8",
+              "Access-Control-Allow-Origin": "*",
+              "Authorization": `Basic ${token}`
+          }
+        };
+
         await axios
-          .post("http://localhost:1337/api/messages", strapiData)//Storing the messages in Strapi
+          .post("https://productivity-tips-strapi-5944.onrender.com/api/messages", strapiData, axiosConfig)//Storing the messages in Strapi
           .then((e) => {
             socket.broadcast.to("group").emit("message", {//Sending the message to the group
               user: data.username,
